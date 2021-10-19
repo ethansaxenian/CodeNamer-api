96d3f8602d760fc210397d94c83690a3a34f7306
Ethan Saxenian <esaxenian@middlebury.edu>
2021-10-19 16:28:36 -0400

    add query param to route

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
-from word_association import load_model, is_valid_clue, get_similar_words
+from word_association import get_similar_words
+from words import WORDS
 
 app = Flask(__name__)
 CORS(app)
@@ -8,13 +9,17 @@ CORS(app)
 
 @app.route("/")
 def home_route():
-    return "hello world"
+    return "<p>CodeNamer api</p>"
 
 
-@app.route("/words/<word>")
-@app.route("/words/<word>/<num>")
+@app.route("/clues/<word>")
+@app.route("/clues/<word>/<num>")
 def get_clues(word, num=100):
-    model = load_model()
-    clues = get_similar_words(model, word, int(num))
-    parsed_words = [(clue, score) for (clue, score) in clues if is_valid_clue(clue, word)]
-    return {new_word.lower(): score for new_word, score in parsed_words}
+    # can specify "valid=false" in query string to return top n clues regardless of validity for codenames
+    wants_valid_clues = not (request.args and request.args.get('valid').lower() == "false")
+    return get_similar_words(word, int(num), wants_valid_clues)
+
+
+@app.route("/words")
+def get_words():
+    return jsonify(WORDS)
--- word_association.py
+++ word_association.py
@@ -1,24 +1,28 @@
 from gensim.models import KeyedVectors
 
-MODEL_NAME = "glove-wiki-gigaword-50"
-KEYED_VECTORS_PATH = f"~/gensim-data/{MODEL_NAME}/{MODEL_NAME}.gz"
-
-
-def load_word_vectors() -> KeyedVectors:
-    return KeyedVectors.load_word2vec_format(KEYED_VECTORS_PATH, binary=True, limit=500000)
-
-
-def get_similar_words(model: KeyedVectors, word: str, number: int = 5) -> list[tuple[str, float]]:
-    return model.most_similar(word, topn=number)
-
-
-def save_model(model: KeyedVectors, name: str):
-    model.save(f'models/{name}')
-
 
 def load_model() -> KeyedVectors:
     return KeyedVectors.load(f'models/wiki-vectors')
 
 
 def is_valid_clue(clue: str, matcher: str) -> bool:
-    return ("_" not in clue.lower()) and (matcher.lower() not in clue.lower()) and (clue.lower() not in matcher.lower())
+    return clue.isalpha() and (matcher.lower() not in clue.lower()) and (clue.lower() not in matcher.lower())
+
+
+def get_similar_words(word: str, number: int = 5, wants_valid_clues: bool = True) -> dict[str, float]:
+    model = load_model()
+
+    if not wants_valid_clues:
+        clues = model.most_similar(word, topn=number)
+        return {new_word.lower(): score for new_word, score in clues}
+
+    temp = number
+    while True:
+        clues = model.most_similar(word, topn=temp)
+        if not wants_valid_clues:
+            return {new_word.lower(): score for new_word, score in clues}
+        parsed_words = [(clue, score) for (clue, score) in clues if is_valid_clue(clue, word)]
+        if len(parsed_words) == number:
+            break
+        temp += 1
+    return {new_word.lower(): score for new_word, score in parsed_words}
