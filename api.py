25d7d64f2f186ebb27389e5110d84c5691c615d8
Ethan Saxenian <esaxenian@middlebury.edu>
2021-10-19 22:02:18 -0400

    adjust routes

--- api.py
+++ api.py
@@ -1,10 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from word_association import get_similar_words
+from word_association import similar_words, read_codenames_board
 from words import WORDS
 
 app = Flask(__name__)
 CORS(app)
+
+
+def parse_query_list(key: str) -> list[str]:
+    result = request.args.get(key)
+    return result.split(" ") if result else []
+
+
+def parse_query_param(key: str, default):
+    result = request.args.get(key)
+    return result or default
 
 
 @app.route("/")
@@ -12,12 +22,22 @@ def home_route():
     return "<p>CodeNamer api</p>"
 
 
-@app.route("/clues/<word>")
-@app.route("/clues/<word>/<num>")
-def get_clues(word, num=100):
+@app.route("/clues")
+def get_clues():
+    positive = parse_query_list("positive")
+    negative = parse_query_list("negative")
+    neutral = parse_query_list("neutral")
+    assassin = parse_query_list("assassin")
+    n = parse_query_param("n", 10)
+    return read_codenames_board(positive, negative, neutral, assassin, int(n))
+
+
+@app.route("/words/<word>")
+def get_similar_words(word):
+    num = parse_query_param("num", 10)
     # can specify "valid=false" in query string to return top n clues regardless of validity for codenames
-    wants_valid_clues = not (request.args and request.args.get('valid').lower() == "false")
-    return get_similar_words(word, int(num), wants_valid_clues)
+    wants_valid_clues = parse_query_param("valid", "true").lower() == "true"
+    return similar_words(word, int(num), wants_valid_clues)
 
 
 @app.route("/words")
--- word_association.py
+++ word_association.py
@@ -5,11 +5,17 @@ def load_model() -> KeyedVectors:
     return KeyedVectors.load(f'models/wiki-vectors')
 
 
-def is_valid_clue(clue: str, matcher: str) -> bool:
-    return clue.isalpha() and (matcher.lower() not in clue.lower()) and (clue.lower() not in matcher.lower())
+def is_valid_clue(clue: str, board: list[str]) -> bool:
+    if not clue.isalpha():
+        return False
+    if any(word.lower() in clue.lower() for word in board):
+        return False
+    if any(clue.lower() in word.lower() for word in board):
+        return False
+    return True
 
 
-def get_similar_words(word: str, number: int = 5, wants_valid_clues: bool = True) -> dict[str, float]:
+def similar_words(word: str, number: int = 5, wants_valid_clues: bool = True) -> dict[str, float]:
     model = load_model()
 
     if not wants_valid_clues:
@@ -19,10 +25,23 @@ def get_similar_words(word: str, number: int = 5, wants_valid_clues: bool = True
     temp = number
     while True:
         clues = model.most_similar(word, topn=temp)
-        if not wants_valid_clues:
-            return {new_word.lower(): score for new_word, score in clues}
-        parsed_words = [(clue, score) for (clue, score) in clues if is_valid_clue(clue, word)]
+        parsed_words = [(clue, score) for (clue, score) in clues if is_valid_clue(clue, [word])]
         if len(parsed_words) == number:
+            break
+        temp += 1
+    return {new_word.lower(): score for new_word, score in parsed_words}
+
+
+def read_codenames_board(positive: list[str], negative: list[str], neutral: list[str], assassin: list[str],
+                         n: int = 10) -> dict[str, float]:
+    model = load_model()
+
+    temp = n
+    while True:
+        clues = model.most_similar(positive=positive, negative=negative + neutral + assassin, topn=temp)
+        parsed_words = [(clue, score) for (clue, score) in clues if
+                        is_valid_clue(clue, positive + negative + neutral + assassin)]
+        if len(parsed_words) == n:
             break
         temp += 1
     return {new_word.lower(): score for new_word, score in parsed_words}
