62878e56bfa3e7aa505c55d8270ff0cff3976e26
Ethan Saxenian <esaxenian@middlebury.edu>
2021-11-19 11:58:13 -0500

    try to make the algorithm more efficient

--- nlp_model.py
+++ nlp_model.py
@@ -20,7 +20,8 @@ CluesResponse = dict[int, list[Clue]]
 
 
 class NLPModel:
-    def __init__(self, model_name: str = "word2vec-google-news-300"):
+    def __init__(self, model_name: str = "word2vec-google-news-300", num_valid_clues_per_word_group: int = 10,
+                 clues_per_size_to_return: int = 5, clue_buffer_size: int = 4096):
         # possible models are fasttext-wiki-news-subwords-300 and word2vec-google-news-300
         self.model: KeyedVectors = KeyedVectors.load(f'models/{model_name}')
         self.model.sort_by_descending_frequency()
@@ -35,12 +36,15 @@ class NLPModel:
         self.model.add_vectors(list(missing_words.keys()), list(missing_words.values()))
 
         # controls how many valid clues the algorithm will generate for each combination
-        self.num_valid_clues_per_word_group = 50
+        self.num_valid_clues_per_word_group = num_valid_clues_per_word_group
 
         # controls how many clues of each size that will be returned in the response object
-        self.clues_per_size_to_return = 5
+        self.clues_per_size_to_return = clues_per_size_to_return
 
-    def smaller_model(self, board: CodenamesBoard, color: str, size: int) -> KeyedVectors:
+        # controls the number of potential clues to generate initially
+        self.clue_buffer_size = clue_buffer_size
+
+    def smaller_model(self, board: CodenamesBoard, color: str, size: int = 10000) -> KeyedVectors:
         """
         returns a model that only includes clues that are positively associated with one color of a Codenames board
         """
@@ -55,7 +59,7 @@ class NLPModel:
 
         # create a much smaller model by only including words that are positively correlated with the good words
         # this will increase efficiency and remove the vast majority of words that don't make sense
-        smaller_model = self.smaller_model(board, color, 10000)
+        smaller_model = self.smaller_model(board, color)
 
         # sort results by clue size
         results_by_number = defaultdict(list)
@@ -66,31 +70,25 @@ class NLPModel:
         for i in [2, 3, 4]:
             for positive_group in itertools.combinations(board.positive(color), i):
 
-                temp = self.num_valid_clues_per_word_group
+                potential_clue_buffer = smaller_model.most_similar(positive=positive_group,
+                                                                   negative=board.negative(color),
+                                                                   topn=self.clue_buffer_size)
+
                 valid_clues = []
 
-                # continue calculating more and more potential clues until we have enough
-                while len(valid_clues) != self.num_valid_clues_per_word_group:
+                for word, score in potential_clue_buffer:
+                    if word.lower() not in invalid_clues and board.is_valid_clue(word):
+                        # if there is an assassin, make sure that any clues are at most orthogonal to it
+                        if not board.has_assassin() or smaller_model.similarity(word, board.assassin) <= 0:
+                            new_clue = Clue(word.lower(), score, [w.lower().replace("_", " ") for w in positive_group])
+                            valid_clues.append(new_clue)
+                    else:
+                        invalid_clues.add(word.lower())
 
-                    # unfortunately we have to generate the same words over and over, which reduces efficiency
-                    # there is no way that I've found to skip over words that have already been generated
-                    words = smaller_model.most_similar(positive=positive_group, negative=board.negative(color), topn=temp)
-
-                    for (word, score) in words:
-                        if word.lower() not in invalid_clues and board.is_valid_clue(word):
-                            # if there is an assassin, make sure that any clues are at most orthogonal to it
-                            if not board.has_assassin() or smaller_model.similarity(word, board.assassin) <= 0:
-                                new_clue = Clue(word.lower(), score, [w.lower().replace("_", " ") for w in positive_group])
-                                valid_clues.append(new_clue)
-                        else:
-                            invalid_clues.add(word.lower())
-
-                        # once we've found enough clues, move on to the next group of words
-                        if len(valid_clues) == self.num_valid_clues_per_word_group:
-                            results_by_number[i].extend(valid_clues)
-                            break
-
-                    temp += 1
+                    # once we've found enough clues, move on to the next group of words
+                    if len(valid_clues) == self.num_valid_clues_per_word_group:
+                        results_by_number[i].extend(valid_clues)
+                        break
 
         return self.parse_clue_results(results_by_number)
 
